5cfa7e8d52a7f37833610e16615c20534e6931a6
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-10-31 18:18:56 +0200

    Store_index_url

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :line_items
   resources :carts
-  root to: 'store#index', as: 'store'
+  root 'store#index', as: 'store_index'
 
   resources :products
 end
