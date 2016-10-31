50842a4e9924ab121ea59214370dfbb89f0e5d05
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-10-31 11:21:42 +0200

    Validation!

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -9,43 +10,53 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
-end
 
-test "product price must be positive" do
- # цена товара должна быть положительной
- product = Product.new(title: "My Book Title",
- description: "yyy",
- image_url: "zzz.jpg")
- product.price = -1
- assert product.invalid?
- assert_equal ["must be greater than or equal to 0.01"],
- product.errors[:price]
- # должна быть больше или равна 0.01
- product.price = 0
- assert product.invalid?
- assert_equal ["must be greater than or equal to 0.01"],
- product.errors[:price]
- product.price = 1
- assert product.valid?
-end
+  test "product price must be positive" do
+    # цена товара должна быть положительной
+    product = Product.new(title: "My Book Title",
+    description: "yyy",
+    image_url: "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    # должна быть больше или равна 0.01
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+    product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
 
-def new_product(image_url)
- Product.new( title: "My Book Title",
- description: "yyy",
- price: 1,
- image_url: image_url)
-end
-test "image url" do
- # url изображения
- ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
- http://a.b.c/x/y/z/fred.gif }
- bad = %w{ fred.doc fred.gif/more fred.gif.more }
- ok.each do |name|
- assert new_product(name).valid?, "#{name} shouldn't be invalid"
- # не должно быть неприемлемым
- end
- bad.each do |name|
- assert new_product(name).invalid?, "#{name} shouldn't be valid"
- # не должно быть приемлемым
- end
+  def new_product(image_url)
+    Product.new( title: "My Book Title",
+    description: "yyy",
+    price: 1,
+    image_url: image_url)
+  end
+  test "image url" do
+    # url изображения
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+    http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be invalid"
+    # не должно быть неприемлемым
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    # не должно быть приемлемым
+    end
+  end
+
+  test " product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+    description: "yyy",
+    price: 1,
+    image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('activerecord.errors.messages.taken')],
+    product.errors[:title]
+  end
 end
