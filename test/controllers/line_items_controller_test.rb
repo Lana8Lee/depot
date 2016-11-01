aa7d07b77def01e5af228cec9f7ae7f309431606
Svetlana Lytvynenko <sveta.litvinenko1407@gmail.com>
2016-11-01 16:29:09 +0200

    Add a Dash of Ajax

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -19,27 +19,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
-    get new_line_item_url#---
-# Excerpted from "Agile Web Development with Rails 5",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
-#---
-require 'test_helper'
-
-class LineItemsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @line_item = line_items(:one)
-  end
-
-  test "should get index" do
-    get line_items_url
-    assert_response :success
-  end
-
-  test "should get new" do
     get new_line_item_url
     assert_response :success
   end
@@ -78,39 +57,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
-end
 
-    assert_response :success
-  end
-
-  test "should create line_item" do
+  test "should create line_item via ajax" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+      post line_items_url, params: { product_id: products(:ruby).id },
+        xhr: true
     end
 
-    assert_redirected_to line_item_url(LineItem.last)
-  end
-
-  test "should show line_item" do
-    get line_item_url(@line_item)
     assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_line_item_url(@line_item)
-    assert_response :success
-  end
-
-  test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
-    assert_redirected_to line_item_url(@line_item)
-  end
-
-  test "should destroy line_item" do
-    assert_difference('LineItem.count', -1) do
-      delete line_item_url(@line_item)
+    assert_select_jquery :html, '#cart' do
+      assert_select 'tr#current_item td', /Programming Ruby 1.9/
     end
-
-    assert_redirected_to line_items_url
   end
 end
